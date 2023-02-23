e95b04ddf039bd8986b42646cf8acf65846fd611
kevinliu-bmb <kevinliu.bmb@gmail.com>
2023-02-23 14:26:43 -0500

    Added cancer selector

--- views/cancer_view.py
+++ views/cancer_view.py
@@ -6,6 +6,9 @@ def generate_cancer_view() -> None:
 
     df = st.session_state["data"]
 
+    mock_cancers = ["Breast Cancer", "Prostate Cancer", "Lung Cancer", "Colorectal Cancer", "Pancreatic Cancer"]
+    cancer = st.selectbox(label="Cancer Type", options=mock_cancers, index=0)
+
     chart = alt.Chart(df).mark_bar().encode(
         x=alt.X('Chromosome:N'),
         y=alt.Y('count(Gene):Q'),
