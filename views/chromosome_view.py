e3b98413f0acb47113ec76c68d987e6a464a9a67
kevinliu-bmb <kevinliu.bmb@gmail.com>
2023-02-24 09:52:54 -0500

    Added demo of chromosome selector

--- views/chromosome_view.py
+++ views/chromosome_view.py
@@ -12,17 +12,19 @@ def generate_chromosome_view() -> None:
     chromosome_names = [f"Chr {i}" for i in chromosome_ids]
 
     # Create chromosome selection widget.
-    chromosome = st.selectbox(label="Chromosome", options=chromosome_names, index=0)
+    chromosome_select = st.selectbox(label="Chromosome", options=chromosome_names, index=0)
 
     df = st.session_state["data"]
 
     # Filter the data to only include the selected chromosome.
-    chromosome_df = df[df["Chromosome"] == chromosome]
+    chromosome_df = df[df["Chromosome"] == chromosome_select[-1:]]
 
     # Place holder for the chromosome visualization.
-    chart = alt.Chart(df).mark_bar().encode(
+    chart = alt.Chart(chromosome_df).mark_bar().encode(
         x=alt.X('Chromosome:N'),
         y=alt.Y('count(Gene):Q'),
         color=alt.Color('Chromosome:N')
     )
+
+    st.altair_chart(chart, use_container_width=True)
 
