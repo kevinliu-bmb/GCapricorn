ba02a7719a6aa04173c50683afa1e76eff34fc0f
kevinliu-bmb <kevinliu.bmb@gmail.com>
2023-02-24 09:38:34 -0500

    Added chromosome selector to Chromosome View

--- views/chromosome_view.py
+++ views/chromosome_view.py
@@ -3,3 +3,7 @@ import streamlit as st
 
 def generate_chromosome_view() -> None:
     st.header("Chromosome View")
+
+    chromosome_ids = [i for i in range(1, 23)].append("X").append("Y")
+    chromosome_names = [f"Chr {i}" for i in chromosome_ids]
+    chromosome = st.selectbox(label="Chromosome", options=chromosome_names, index=0)
