eea1b5593a9dac2f31ed33c64cc87b2afb3c0f01
kevinliu-bmb <kevinliu.bmb@gmail.com>
2023-02-24 09:43:45 -0500

    Updated chromosomes list

--- views/chromosome_view.py
+++ views/chromosome_view.py
@@ -5,6 +5,7 @@ def generate_chromosome_view() -> None:
     st.header("Chromosome View")
 
     chromosome_ids = [i for i in range(1, 23)]
-    chromosome_ids.append("X").append("Y") 
+    chromosome_ids.append("X")
+    chromosome_ids.append("Y")
     chromosome_names = [f"Chr {i}" for i in chromosome_ids]
     chromosome = st.selectbox(label="Chromosome", options=chromosome_names, index=0)
