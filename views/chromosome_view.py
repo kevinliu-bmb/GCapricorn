6e3d15dee06c3a7d905a018e17874a4a955ee6ea
kevinliu-bmb <kevinliu.bmb@gmail.com>
2023-02-25 18:28:05 -0500

    Added get min and max chr pos steps

--- views/chromosome_view.py
+++ views/chromosome_view.py
@@ -37,11 +37,22 @@ def generate_chromosome_view() -> None:
     if df_chromosome_filt.empty:
         st.write("No data to display.")
     else:
+        # Convert the Position column to a list of start and end positions.
+        chr_pos = df_chromosome_filt["Position"].str.split("-", expand=True)
+
+        # get the minimal start position and maximal end position
+        min_start = chr_pos[0].astype(int).min()
+        max_end = chr_pos[1].astype(int).max()
+
         chart = alt.Chart(df_chromosome_filt).mark_bar().encode(
             x=alt.X('Chromosome:N'),
             y=alt.Y('count(Gene):Q'),
             color=alt.Color('Chromosome:N')
-        )
+        ) & alt.Chart(df_chromosome_filt).mark_bar().encode(
+            x=alt.X('Chromosome:N'),
+            y=alt.Y('count(Gene):Q'),
+            color=alt.Color('Chromosome:N')
+        )       
 
         st.altair_chart(chart, use_container_width=True)
 
