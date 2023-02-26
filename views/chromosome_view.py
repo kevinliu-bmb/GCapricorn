8279963b823600239250baf0f6eeba3378b23781
kevinliu-bmb <kevinliu.bmb@gmail.com>
2023-02-26 10:31:34 -0500

    Added function to generate simulated data

--- views/chromosome_view.py
+++ views/chromosome_view.py
@@ -5,7 +5,57 @@ import streamlit as st
 import altair as alt
 
 
+def generate_simulated_data() -> pd.DataFrame:
+    """
+    Generate simulated data for testinging the chromosome view.
+    :return: A DataFrame with simulated data.
+    """
+    # Create a dictionary with three keys: 'position', 'value', and 'cancer'
+    sim_data = {
+        'position': range(1, 1000001),
+        'value': [10] * 1000000
+    }
+
+    # Create a DataFrame from the dictionary
+    df_mock = pd.DataFrame(sim_data)
+
+    # Add a column 'cancer' to the DataFrame with values of "N" initially
+    df_mock['cancer'] = 'N'
+
+    # Set maximum of 10000 'Y's
+    max_Y = 100000
+
+    # Set consecutive 'Y's in each 1000 rows
+    consecutive_Y = 10000
+
+    # Calculate the number of groups of consecutive 'Y's
+    num_groups = max_Y // consecutive_Y
+
+    # Calculate the minimum number of rows between each group of consecutive 'Y's
+    min_row_diff = (len(df_mock) - consecutive_Y * num_groups) // (num_groups + 1)
+
+    # Calculate the starting index for each group of consecutive 'Y's
+    start_indices = [i * (consecutive_Y + min_row_diff) + min_row_diff for i in range(num_groups)]
+
+    # Get random indices to set 'Y' values
+    y_indices = np.random.choice(df_mock.index, size=max_Y, replace=False)
+
+    # Split the 'y_indices' into groups of 1000
+    y_indices_groups = np.array_split(y_indices, num_groups)
+
+    # Set consecutive 'Y's in each group
+    for i, group in enumerate(y_indices_groups):
+        start_idx = start_indices[i]
+        df_mock.loc[start_idx:start_idx + consecutive_Y - 1, 'cancer'] = 'Y'
+
+    return df_mock
+
+
 def generate_chromosome_view() -> None:
+    """
+    Generates the chromosome view.
+    :return: None.
+    """
     st.header("Chromosome View")
 
     df = st.session_state["data"]
@@ -56,44 +106,7 @@ def generate_chromosome_view() -> None:
         #     color=alt.Color('Chromosome:N')
         # )
 
-        # Create a dictionary with three keys: 'position', 'value', and 'cancer'
-        sim_data = {
-            'position': range(1, 1000001),
-            'value': [10] * 1000000
-        }
-
-        # Create a DataFrame from the dictionary
-        df_mock = pd.DataFrame(sim_data)
-
-        # Add a column 'cancer' to the DataFrame with values of "N" initially
-        df_mock['cancer'] = 'N'
-
-        # Set maximum of 10000 'Y's
-        max_Y = 100000
-
-        # Set consecutive 'Y's in each 1000 rows
-        consecutive_Y = 10000
-
-        # Calculate the number of groups of consecutive 'Y's
-        num_groups = max_Y // consecutive_Y
-
-        # Calculate the minimum number of rows between each group of consecutive 'Y's
-        min_row_diff = (len(df_mock) - consecutive_Y * num_groups) // (num_groups + 1)
-
-        # Calculate the starting index for each group of consecutive 'Y's
-        start_indices = [i * (consecutive_Y + min_row_diff) + min_row_diff for i in range(num_groups)]
-
-        # Get random indices to set 'Y' values
-        y_indices = np.random.choice(df_mock.index, size=max_Y, replace=False)
-
-        # Split the 'y_indices' into groups of 1000
-        y_indices_groups = np.array_split(y_indices, num_groups)
-
-        # Set consecutive 'Y's in each group
-        for i, group in enumerate(y_indices_groups):
-            start_idx = start_indices[i]
-            df_mock.loc[start_idx:start_idx + consecutive_Y - 1, 'cancer'] = 'Y'
-
+        df_mock = generate_simulated_data()
 
         chart = alt.Chart(df_chromosome_filt).mark_bar().encode(
             x=alt.X('Chromosome:N'),
