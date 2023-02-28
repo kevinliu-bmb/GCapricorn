2237d46336897324daa428824688bfdc4f81c51f
Diego Trujillo <diegotrujillojimenez@hms.harvard.edu>
2023-02-28 16:54:46 -0500

    Improvements to chromosome view

--- views/chromosome_view.py
+++ views/chromosome_view.py
@@ -1,6 +1,51 @@
-
+import pandas as pd
 import streamlit as st
 import altair as alt
+
+
+def build_chromosome_chart(chromosome_proteins: pd.DataFrame) -> alt.Chart:
+    """
+    Generates the chromosome Altair chart.
+    :param chromosome_proteins: DataFrame containing chromosome/protein information.
+    :return: None.
+    """
+
+    brush = alt.selection(type="interval", encodings=["x"]) # BUG: when hovering over brushed region, displays "True" in the tooltip.
+
+    top_line = alt.Chart(chromosome_proteins).mark_line().encode(
+        x=alt.X("start_position", type="quantitative", scale=alt.Scale(domain=brush.ref())),
+        tooltip=["Gene", "start_position", "end_position"]
+    )
+
+    gene_details = alt.Chart(chromosome_proteins).mark_circle(size=50).encode(
+        x=alt.X("start_position", type="quantitative", scale=alt.Scale(domain=brush.ref()), title="Chromosomal Position"),
+        color="Protein class:N",
+        tooltip=["Gene", "start_position", "end_position", "Protein class"]
+    )
+
+    detailed_view = (top_line + gene_details).properties(
+        width=600,
+        height=150
+    )
+
+    bottom_line = alt.Chart(chromosome_proteins).mark_line().encode(
+        x="start_position:Q",
+        tooltip=["Gene", "start_position", "end_position"]
+    )
+
+    gene_overview = alt.Chart(chromosome_proteins).mark_rect(size=10).encode(
+        x=alt.X("start_position", type="quantitative", title="Drag to zoom"),
+        x2="end_position:Q",
+        tooltip=["Gene", "start_position", "end_position"],
+        color=alt.Color("Protein class:N") # TODO: color by selected protein classes; BUG: not displaying the simplified selected protein classes.
+    )
+
+    general_view = (bottom_line + gene_overview).properties(
+        width=600,
+        height=50,
+    ).add_selection(brush)
+
+    return detailed_view & general_view
 
 
 def generate_chromosome_view() -> None:
@@ -8,6 +53,7 @@ def generate_chromosome_view() -> None:
     Generates the chromosome view.
     :return: None.
     """
+
     st.header("Chromosome View")
 
     data = st.session_state["data"]
@@ -31,49 +77,5 @@ def generate_chromosome_view() -> None:
     chromosome_proteins["start_position"] = chromosome_proteins["Position"].str.split("-", expand=True)[0]
     chromosome_proteins["end_position"] = chromosome_proteins["Position"].str.split("-", expand=True)[1]
 
-    brush = alt.selection(type="interval", encodings=["x"]) # BUG: when hovering over brushed region, displays "True" in the tooltip.
-
-    chart_chromosome = alt.Chart(chromosome_proteins).mark_line().encode(
-        x="start_position:Q",
-        tooltip=["Gene", "start_position", "end_position"]
-    )
-
-    chart_genes = alt.Chart(chromosome_proteins).mark_rect(size=10).encode(
-        x="start_position:Q",
-        x2="end_position:Q",
-        tooltip=["Gene", "start_position", "end_position"],
-        color=alt.Color("Protein class:N") # TODO: color by selected protein classes; BUG: not displaying the simplified selected protein classes.
-    )
-
-    chart_bottom = (chart_chromosome + chart_genes).properties(
-        width=600,
-        height=50,
-        title=f"Brush over to only display the selected region on Chromosome {chromosome_select}"
-    ).add_selection(brush)
-
-    chart_chromosome_top = alt.Chart(chromosome_proteins).mark_line().encode(
-        x="start_position:Q",
-        tooltip=["Gene", "start_position", "end_position"]
-    ).properties(
-        width=600,
-        height=150
-    ).transform_filter(brush)
-
-    chart_selected_genes = alt.Chart(chromosome_proteins).mark_circle(size=50).encode(
-        x="start_position:Q",
-        color="Protein class:N",
-        tooltip=["Gene", "start_position", "end_position", "Protein class"]
-    ).properties(
-        width=600,
-        height=50,
-        title=f"Scroll to zoom into the chromosomal region"
-    ).transform_filter(brush).interactive() # TODO: fix the horizontal pan to minimum value of 0 and update based on the brush selection.
-
-    chart_top = (chart_chromosome_top + chart_selected_genes)
-
-    chart = (chart_top & chart_bottom).configure_axis(
-        titleFontSize=0 # BUG: not hiding the axis title.
-    )
-
+    chart = build_chromosome_chart(chromosome_proteins)
     st.altair_chart(chart, use_container_width=True)
-
