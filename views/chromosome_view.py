2f20a9c6191c98ef225c4bc0dee6b1b8e77ebb4d
Diego Trujillo <diegotrujillojimenez@hms.harvard.edu>
2023-03-01 12:19:38 -0500

    Fix primary protein class issue

--- views/chromosome_view.py
+++ views/chromosome_view.py
@@ -2,6 +2,12 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 
+# TODO: Add all available protein protein_classes, sorted by priority.
+# The class with the highest priority will be the one chosen for the protein as its class.
+protein_class_priority = [
+    "Enzymes",
+    "Transporters"
+]
 
 @st.cache_data
 def build_chromosome_chart(chromosome_proteins: pd.DataFrame) -> alt.Chart:
@@ -14,14 +20,14 @@ def build_chromosome_chart(chromosome_proteins: pd.DataFrame) -> alt.Chart:
     brush = alt.selection(type="interval", encodings=["x"]) # BUG: when hovering over brushed region, displays "True" in the tooltip.
 
     top_line = alt.Chart(chromosome_proteins).mark_line().encode(
-        x=alt.X("start_position:Q", scale=alt.Scale(domain=brush.ref())),
-        tooltip=["Gene", "start_position", "end_position"]
+        x=alt.X("Start Position:Q", scale=alt.Scale(domain=brush.ref())),
+        tooltip=["Gene", "Start Position", "End Position"]
     )
 
     gene_details = alt.Chart(chromosome_proteins).mark_circle(size=50).encode(
-        x=alt.X("start_position:Q", scale=alt.Scale(domain=brush.ref()), title="Chromosomal Position"),
-        color=alt.Color("Protein class:N"),
-        tooltip=["Gene", "Gene synonym", "Protein class", "Ensembl", "Uniprot", "Biological process", "start_position", "end_position"]
+        x=alt.X("Start Position:Q", scale=alt.Scale(domain=brush.ref()), title="Chromosomal Position"),
+        color=alt.Color("Primary Protein Class:N"),
+        tooltip=["Gene", "Gene synonym", "Protein class", "Ensembl", "Uniprot", "Biological process", "Start Position", "End Position"]
     )
 
     detailed_view = (top_line + gene_details).properties(
@@ -30,15 +36,15 @@ def build_chromosome_chart(chromosome_proteins: pd.DataFrame) -> alt.Chart:
     )
 
     bottom_line = alt.Chart(chromosome_proteins).mark_line().encode(
-        x=alt.X("start_position:Q"),
-        tooltip=["Gene", "start_position", "end_position"]
+        x=alt.X("Start Position:Q"),
+        tooltip=["Gene", "Start Position", "End Position"]
     )
 
     gene_overview = alt.Chart(chromosome_proteins).mark_rect(size=10).encode(
-        x=alt.X("start_position:Q", title="Drag to select chromosomal region, scroll to zoom in/out"),
-        x2=alt.X2("end_position:Q"),
-        tooltip=["Gene", "start_position", "end_position"],
-        color=alt.Color("Protein class:N") # TODO: color by selected protein classes; BUG: not displaying the simplified selected protein classes.
+        x=alt.X("Start Position:Q", title="Drag to select chromosomal region, scroll to zoom in/out"),
+        x2=alt.X2("End Position:Q"),
+        tooltip=["Gene", "Start Position", "End Position"],
+        color=alt.Color("Primary Protein Class:N") # TODO: color by selected protein protein_classes; BUG: not displaying the simplified selected protein protein_classes.
     )
 
     general_view = (bottom_line + gene_overview).properties(
@@ -47,6 +53,19 @@ def build_chromosome_chart(chromosome_proteins: pd.DataFrame) -> alt.Chart:
     ).add_selection(brush)
 
     return detailed_view & general_view
+
+
+def get_primary_protein_class(protein_classes: str) -> str:
+    """
+    Retrieve the primary protein class from a comma-separated string of protein classes.
+    :param protein_classes: Protein classes in a comma-separated format.
+    :return: The primary protein class as a string.
+    """
+    class_list = [x.strip() for x in protein_classes.split(",")]
+    prioritized_protein_classes = [x for x in protein_class_priority if x in class_list]
+    if prioritized_protein_classes:
+        return prioritized_protein_classes[0]
+    return class_list[0]
 
 
 def generate_chromosome_view() -> None:
@@ -60,7 +79,8 @@ def generate_chromosome_view() -> None:
     data = st.session_state["data"]
 
     cancer_selection = st.session_state["cancer_selection"]
-    # TODO: filter to only include proteins and chromosomes associated with the selected cancer type.
+    data = data[(data["Unfavorable prognostics"].apply(lambda x: cancer_selection in x)) |
+                (data["Favorable prognostics"].apply(lambda x: cancer_selection in x))]
 
     protein_selection = st.session_state["protein_selection"]
 
@@ -68,15 +88,17 @@ def generate_chromosome_view() -> None:
     chromosome_data = data[data["Chromosome"] == chromosome_select]
 
     if not protein_selection:
-        st.warning("No protein classes selected. Displaying all proteins in the chromosome.")
+        st.warning("No protein protein_classes selected. Displaying all proteins in the chromosome.")
         chromosome_proteins = chromosome_data
     else:
         chromosome_proteins = chromosome_data[chromosome_data["Protein class"].apply(
             lambda x: any(protein_class in x for protein_class in protein_selection)
         )]
 
-    chromosome_proteins["start_position"] = chromosome_proteins["Position"].str.split("-", expand=True)[0]
-    chromosome_proteins["end_position"] = chromosome_proteins["Position"].str.split("-", expand=True)[1]
+    chromosome_proteins["Start Position"] = chromosome_proteins["Position"].str.split("-", expand=True)[0]
+    chromosome_proteins["End Position"] = chromosome_proteins["Position"].str.split("-", expand=True)[1]
+
+    chromosome_proteins["Primary Protein Class"] = chromosome_proteins["Protein class"].apply(get_primary_protein_class)
 
     chart = build_chromosome_chart(chromosome_proteins)
     st.altair_chart(chart, use_container_width=True)
