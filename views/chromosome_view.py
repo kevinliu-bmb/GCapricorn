a888826e8b43ecc986aeb181335462b6fd2286ce
Diego Trujillo <diegotrujillojimenez@hms.harvard.edu>
2023-03-01 15:35:23 -0500

    Fix protein class prioritization process

--- views/chromosome_view.py
+++ views/chromosome_view.py
@@ -55,14 +55,16 @@ def build_chromosome_chart(chromosome_proteins: pd.DataFrame) -> alt.Chart:
     return detailed_view & general_view
 
 
-def get_primary_protein_class(protein_classes: str) -> str:
+def get_primary_protein_class(protein_classes: str, protein_selection: list[str]) -> str:
     """
     Retrieve the primary protein class from a comma-separated string of protein classes.
     :param protein_classes: Protein classes in a comma-separated format.
+    :param protein_selection: list of selected protein classes to filter by.
     :return: The primary protein class as a string.
     """
     class_list = [x.strip() for x in protein_classes.split(",")]
     prioritized_protein_classes = [x for x in protein_class_priority if x in class_list]
+    prioritized_protein_classes = list(filter(lambda x: x in protein_selection, prioritized_protein_classes))
     if prioritized_protein_classes:
         return prioritized_protein_classes[0]
     return class_list[0]
@@ -98,7 +100,9 @@ def generate_chromosome_view() -> None:
     chromosome_proteins["Start Position"] = chromosome_proteins["Position"].str.split("-", expand=True)[0]
     chromosome_proteins["End Position"] = chromosome_proteins["Position"].str.split("-", expand=True)[1]
 
-    chromosome_proteins["Primary Protein Class"] = chromosome_proteins["Protein class"].apply(get_primary_protein_class)
+    chromosome_proteins["Primary Protein Class"] = chromosome_proteins["Protein class"].apply(
+        lambda x: get_primary_protein_class(x, protein_selection)
+    )
 
     chart = build_chromosome_chart(chromosome_proteins)
     st.altair_chart(chart, use_container_width=True)
